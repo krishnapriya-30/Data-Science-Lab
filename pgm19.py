00e28bf3cbb15e1270fb55c249789fd2c030e4e8
krishnapriya-30 <kichukrishnapriya192@gmail.com>
2026-09-01 11:18:10 +0530

    Add files via upload

---
+++ pgm19.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot
+rollno=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+marks=[22,87,5,43,56,73,55,54,11,20,51,5,79,31,77]
+fig,ax=pyplot.subplots(1,1)
+ax.scatter(rollno,marks)
+ax.set_title("scatter plot")
+ax.set_xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
+ax.set_yticks([25,50,75,100])
+ax.set_xlabel("rollno")
+ax.set_ylabel("marks")
+pyplot.show()
